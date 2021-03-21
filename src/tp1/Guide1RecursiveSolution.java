ccacce81d3ff48cd7ccb1a32ad41dea8723e1ed6
fpochat02 <fpochat@losmolinos.edu.ar>
2021-03-21 16:38:43 -0300

    Termine la parte recursiva del tp1

--- src/tp1/Guide1RecursiveSolution.java
+++ src/tp1/Guide1RecursiveSolution.java
@@ -31,7 +31,23 @@ public class Guide1RecursiveSolution implements Guide1 {
 
     @Override
     public int exercise_1_f(int n) {
-        throw new UnsupportedOperationException("TODO");
+        return auxexercise_1_f(n,0,0);
+    }
+
+    public int auxexercise_1_f(int n, int i, int sum){
+        if (i <= n) {
+            sum += Math.pow(i, 3);
+            return auxexercise_1_f(n, i+1, sum);
+        }
+        return sum;
+    }
+
+    public int exercise_1_f(int n, int i, int sum){
+        if (i <= n) {
+            sum += Math.pow(i, 3);
+            return exercise_1_f(n, i+1, sum);
+        }
+        return sum;
     }
 
     @Override
@@ -46,7 +62,13 @@ public class Guide1RecursiveSolution implements Guide1 {
 
     @Override
     public int exercise_2_c(int n) {
-        throw new UnsupportedOperationException("TODO");
+        if (n == 0){
+            return 0;
+        }else if (n == 1){
+            return 1;
+        }else {
+            return exercise_2_c(n-1) + exercise_2_c(n-2);
+        }
     }
 
     @Override
@@ -61,7 +83,17 @@ public class Guide1RecursiveSolution implements Guide1 {
 
     @Override
     public boolean exercise_4(int[] array) {
-        throw new UnsupportedOperationException("TODO");
+        return auxexercise_4(array,0);
+    }
+
+    public boolean auxexercise_4(int[] elements, int i){
+        if (i < elements.length) {
+            if (elements[i] != elements[elements.length-i-1]) {
+                return false;
+            }
+            return auxexercise_4(elements, i+1);
+        }
+        return true;
     }
 
     @Override
@@ -81,7 +113,18 @@ public class Guide1RecursiveSolution implements Guide1 {
 
     @Override
     public int exercise_6_b_iii(int n) {
-        throw new UnsupportedOperationException("TODO");
+        return auxexercise_6_b_iii(n,0,2);
+    }
+
+    public int auxexercise_6_b_iii(int n, int amount, int factor){
+        if(factor > n){
+            return amount;
+        }else if (n % factor == 0){
+            n = n/factor;
+            return auxexercise_6_b_iii(n,amount+1, factor);
+        }else if(factor > 2) {  // como el unico numero primo par es el 2, se saltea los numeros pares mayores a este.
+            return auxexercise_6_b_iii(n, amount, factor + 2);
+        }else return auxexercise_6_b_iii(n, amount, factor + 1);
     }
 
     @Override
@@ -91,6 +134,15 @@ public class Guide1RecursiveSolution implements Guide1 {
 
     @Override
     public int exercise_8(int[] coefs, int n) {
-        throw new UnsupportedOperationException("TODO");
+        return auxexercise_8(coefs,n,coefs.length-1,0);
+    }
+
+    private int auxexercise_8(int[] coefs, int n, int i, int result){
+        if (i < 0){
+            return result;
+        }else {
+            result = result*n + coefs[i];
+            return auxexercise_8(coefs,n,i-1,result);
+        }
     }
 }
